3ef51505ddd890d72915ef73d92bee2d33af027c
akosfi <fiakos1997@gmail.com>
2019-09-26 14:37:21 +0200

    added redux store

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,20 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import configureStore from "./store";
 import {Provider} from "react-redux";
+import {Router, Route} from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 
+const history = createBrowserHistory();
 
 ReactDOM.render(
     <Provider store={configureStore()}>
-        <App />
+        <Router history={history}>
+            <div>
+                <div>
+                    <Route exact path="/" component={App} />
+                </div>
+            </div>
+        </Router>
     </Provider>,
     document.getElementById('root')
 );
